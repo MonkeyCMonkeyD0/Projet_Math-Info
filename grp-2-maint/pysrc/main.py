02714436d542407081aeac36787599988ca5025b
QuartoTR <tristan.michel@etu.umpc.fr>
2020-04-05 19:45:14 +0200

    Ajout import dans main.py

---
+++ grp-2-maint/pysrc/main.py
@@ -0,0 +1,11 @@
+from tools.axis.Axis import Axis
+from tools.cnds_initials.Initials import Initials
+from tools.line_style_form.Color import Color
+from tools.line_style_form.Form import Form
+from tools.line_style_form.LineStyle import LineStyle
+from tools.phase_diag.PhaseDiag import PhaseDiag
+
+from modls.LotkaVolterra import LotkaVolterra
+from modls.LotkaVolterra_2 import LotkaVolterra_2
+from modls.SysNLine import SysNLine
+from modls.SysLine import SysLine
